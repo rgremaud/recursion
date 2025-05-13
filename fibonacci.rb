fac49cfc3c7e8e2a0a3f6060f6939c59015260aa
Romain Gremaud <rgremau@gmail.com>
2025-05-13 15:27:10 -0400

    Finish fibonacci seq script

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,7 +1,3 @@
-# Assignment
-# 1 - Using iteration, write a method #fibs which takes a number and returns an array containing
-# that many numbers from the Fibonacci sequence.
-# Using an example input of 8, this function should return the array [0, 1, 1, 2, 3, 5, 8, 13].
 def fibonacci(n) # rubocop:disable Metrics/MethodLength
   fibs_array = [0, 1]
   if n == 1
@@ -20,30 +16,15 @@ def fibonacci(n) # rubocop:disable Metrics/MethodLength
   puts fibs_array
 end
 
-# puts 'fibonacci(8) is: '
-# puts fibonacci(8)
+puts fibonacci(8)
 
-# 2 - Now write another method #fibs_rec which solves the same problem recursively.
-# Using an example input of 8, this function should return the array [0, 1, 1, 2, 3, 5, 8, 13]
+def fibs_rec(n)
+  return [0] if n == 1
+  return [0, 1] if n == 2
 
-def fibs_rec(n, array = [])
-  return n if n < 2
-
-  fibs_rec(n - 1) + fibs_rec(n - 2)
+  fibs_array = fibs_rec(n - 1)
+  fibs_array << fibs_array[-1] + fibs_array[-2]
+  fibs_array
 end
 
-# puts fibs_rec(5)
-
-# 3 - Test both versions of your methods by passing in various lengths as arguments.
-
-def fibs_test(n, fib_rec_array = [])
-  if n.zero?
-    puts fib_rec_array
-  else
-    p fib_rec_array << fibs_rec(n - 1)
-    # fibs_rec(n - 1)
-    fibs_test(n - 1)
-  end
-end
-
-p fibs_test(8, [])
+puts fibs_rec(8)
