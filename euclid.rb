5ce4c2d9a5dc0b74a6bb4c15047a6f8dd45c5e47
Romain Gremaud <romaingremaud@mac.attlocal.net>
2025-05-15 22:02:36 -0400

    Add euclid

---
+++ euclid.rb
@@ -0,0 +1,12 @@
+# involves finding greatest common denominator of two numbers
+# GCD(A,B)
+# if A = 0, GCD = B
+# if B = 0, GCD = A
+# A = B * QUOTIENT + REMAINDER or A = B * Q + R
+# Example, find GCD of A = 270 and B = 192
+# 270 = 192 * 1 + 78
+# 192 = 78 * 2 + 36
+# 78 = 36 * 2 + 6
+# 36 = 6 * 6 + 0
+# A = 6, B = 0
+# GCD = 6
